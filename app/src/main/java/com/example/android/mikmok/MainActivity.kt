94ee1e3fdb8da7c4555aaf0e49f0474dbec2ac54
Ahmad Batal <ahmad77a44k@gmail.com>
2022-08-01 11:48:34 +0300

    add okhttp library and request using okhttp function

--- app/src/main/java/com/example/android/mikmok/MainActivity.kt
+++ app/src/main/java/com/example/android/mikmok/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.android.mikmok
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+
+
+    private fun requestUsingOkHttp(){
+        val request = Request.Builder().url("https://raw.githubusercontent.com/android/tv-samples/main/ClassicsKotlin/app/src/main/assets/media-feed.json").build()
+        client.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+//                runOnUiThread {
+//                }
+            }
+        })
     }
 }
